16d84ad4a8a3e55c82d0383742ff8f530f7a8f0e
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-21 21:45:06 +0000

    Switched database to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,16 +2,33 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Mongo setup:
+const {
+  MongoClient
+} = require('mongodb');
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, client) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+let db = client.db('tweeter');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -32,3 +49,4 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+})
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,20 +8,13 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+    saveTweet: function (newTweet, callback) {
+        db.collection("tweets").insertOne(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+     getTweets: function (callback) {
+      db.collection("tweets").find().toArray(callback);
     }
-
-  };
+  }
 }
